dd8c052fe6c80491397f5fc7d6293afaa7f8d593
nikink334 <nikink334@gmail.com>
2024-01-16 23:11:51 +0530

    order.html

--- js/order.js
+++ js/order.js
@@ -1,4 +1,18 @@
-  
-         function showSlides() {
-            let i;
-         }
+
+
+function confirmBooking() {
+   const isConfirmed = confirm("Are you sure you want to confirm the booking?");
+
+   if (isConfirmed) {
+       const fullName = document.getElementById("fullName").value;
+       const delAddress = document.getElementById("delAddress").value;
+       const gEmail = document.getElementById("gEmail").value;
+
+       alert(`Booking Confirmed!\nName: ${fullName}  \nDelivery Address: ${delAddress} \nEmail: ${gEmail}.`);
+   }
+}
+
+document.getElementById("confirmBooking").addEventListener("click", function(event) {
+   event.preventDefault();
+   confirmBooking();
+});
